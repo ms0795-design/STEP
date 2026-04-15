8ac0bb6ede9449d255f9a6e13262fc5392103881
ms0795 <ms0795@srmist.edu.in>
2026-04-15 22:08:23 +0530

    UC04

--- Train Consist Management App/src/TrainConsistManagenemtApp.java
+++ Train Consist Management App/src/TrainConsistManagenemtApp.java
@@ -1,28 +1,17 @@
-import java.util.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class TrainConsistManagenemtApp {
     public static void main(String[] args) {
-        LinkedList<String> trainConsist = new LinkedList<>();
+        Set<String> trainFormation = new LinkedHashSet<>();
 
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
 
-        trainConsist.add(2, "Pantry Car");
+        trainFormation.add("Sleeper");
 
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
-
-        TreeSet<String> uniqueBogieIDs = new TreeSet<>();
-        String[] inputIDs = {"B101", "B105", "B102", "B101", "B103", "B105"};
-
-        for (String id : inputIDs) {
-            uniqueBogieIDs.add(id);
-        }
-
-        System.out.println("Final Ordered Train Consist: " + trainConsist);
-        System.out.println("Unique & Sorted Bogie IDs: " + uniqueBogieIDs);
+        System.out.println("Final Train Formation: " + trainFormation);
     }
 }
